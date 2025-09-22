0d9edb5641e0b618b4bcc8619d1e91f00522cd62
maryam <maryamabdallahshalash@gmail.com>
2025-09-22 12:52:23 +0300

    fixed cahce embedder error

--- backend/models/embedders/hf_embedder.py
+++ backend/models/embedders/hf_embedder.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 
 class HFEmbedder(BaseEmbedder):
     def __init__(self, model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", device="cpu"):
-        self.model = SentenceTransformer(model_name, device=DEVICE, cache=CACHE_DIR)
+        self.model = SentenceTransformer(model_name, device=DEVICE, cache_folder=CACHE_DIR)
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
         """Embed a list of documents (for storing in vector DB)."""
